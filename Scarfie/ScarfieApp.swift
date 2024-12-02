74e2e0b532e362737d50b212377c2ac1e32924e0
Dayan A <codayanabdulla@gmail.com>
2024-12-02 17:54:41 -0500

    Initial Commit

---
+++ Scarfie/ScarfieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScarfieApp.swift
+//  Scarfie
+//
+//  Created by Dayan Abdulla on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ScarfieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
