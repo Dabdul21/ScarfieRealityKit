973f720f62c071900e14d9e90dc70fab036d93a2
Dayan A <codayanabdulla@gmail.com>
2024-12-30 18:27:52 -0500

    Added to do list

---
+++ Scarfie/TODO.swift
@@ -0,0 +1,12 @@
+//
+//  TODO.swift
+//  Scarfie
+//
+//  Created by Dayan Abdulla on 12/30/24.
+//
+//
+//Now that it detects faces and will switch UI when a face is not detected you will need the following things:
+
+//Models of scarves
+//Loading a 3D model of a scarf.
+//Attaching it to the face anchor detected by ARKit.
