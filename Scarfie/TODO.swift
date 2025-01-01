e3c6149f5ce79cdc8277ab00520f62a5d6443a67
Dayan A <codayanabdulla@gmail.com>
2025-01-01 17:03:58 -0500

    format

--- Scarfie/TODO.swift
+++ Scarfie/TODO.swift
@@ -6,22 +6,23 @@
 //
 //
 //Now that it detects faces and will switch UI when a face is not detected you will need the following things:
-
-//Models of scarves
-//Loading a 3D model of a scarf.
-//Attaching it to the face anchor detected by ARKit.
-//train model
-//find pictures
-//figure out coreml
-//find datapoints
-//add scarf ui
-//add welcome screen
-//maybe app icon
-
-//--------------------------------------
-//              TO-DO LIST CHAT GPT
 /*
-  - Phase 1: Setup Basic Face and Head Detection -
+        Models of scarves
+        Loading a 3D model of a scarf.
+        Attaching it to the face anchor detected by ARKit.
+        train model
+        find pictures
+        figure out coreml
+        find datapoints
+        add scarf ui
+        add welcome screen
+        maybe app icon
+*/
+//--------------------------------------
+/*
+ TO-DO LIST CHAT GPT
+  
+ - Phase 1: Setup Basic Face and Head Detection -
 
      1. Integrate ARKit for Face Tracking:
  Ensure ARKit detects faces reliably using ARFaceTrackingConfiguration.
